379c77e5de470bac78c3ea2a83573e527f02a1eb
Jonas <jonaso@web.de>
2023-04-03 14:59:49 +0300

    custom 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,20 @@
+import styles from "./errorpage.module.css"
+
 import React from 'react'
 import { Button, Result } from 'antd'
 
 
 const Page404: React.FC = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary">Back Home</Button>}
-  />
+  <div className={styles.main}>
+    <div className={styles.container}>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={<Button href="/" type="primary">Back Home</Button>}
+      />
+    </div>
+  </div>
 )
 
 export default Page404
